58c17944d5b47ce4e088b7eb952f24e490713309
Jordy Schreuders <jordy@schreuders.it>
2018-04-18 15:44:24 +0200

    Allow querying /Emails endpoint
    All API calls are Mocked using Guzzle Mock

---
+++ back/app/src/http/InsightlyAPI.php
@@ -0,0 +1,52 @@
+<?php declare(strict_types=1);
+
+namespace Acme\Http;
+
+require_once '/var/www/html/app/vendor/autoload.php';
+
+use \GuzzleHttp\Client;
+use \GuzzleHttp\Exception\RequestException;
+use \GuzzleHttp\Exception\ClientException;
+use \Psr\Http\Message\ResponseInterface;
+
+final class InsightlyAPI {
+
+  /**
+   * @var \GuzzleHttp\Client
+   */
+  private $client;
+
+  private $version = 'v2.2';
+
+  public function __construct(\GuzzleHttp\Client $client) {
+    $this->client = $client;
+  }
+  /**
+   * getEmails
+   *
+   * Returns a batched list of emails. We will use this to
+   * loop over and get specific emails as getEmails does not
+   * contain the full body text.
+   *
+   * @return array
+   */
+  public function getEmails(string $queryString = '', array $options = []): array {
+    return $this->get('Emails' . $queryString . '&brief=true', $options);
+  }
+  /**
+   * Create and send a HTTP GET request
+   *
+   * @param string  $uri the URI to hit
+   * @param array   $options HTTP client configuration options
+   *
+   * @return ResponseInterface
+   *
+   * @throws ClientException
+   * @throws RequestException
+   */
+  private function get(string $uri, array $options = []) {
+    $response = $this->client->get('/' . $this->version . '/' . $uri, $options);
+
+    return json_decode($response->getBody()->getContents());
+  }
+}
---
+++ back/app/tests/InsightlyAPITest.php
@@ -0,0 +1,60 @@
+<?php declare(strict_types=1);
+
+use \PHPUnit\Framework\TestCase;
+use \GuzzleHttp\Handler\MockHandler;
+use \GuzzleHttp\HandlerStack;
+use \GuzzleHttp\Client;
+use \GuzzleHttp\Psr7\Response;
+use \GuzzleHttp\Exception\ClientException;
+use \Acme\Http\InsightlyAPI;
+
+class InsighlyAPITest extends TestCase {
+
+  /**
+   * InsightlyAPI->getEmails(); 
+   */
+  public function test_getEmails_returns_a_paginated_array_containing_json_objects() {
+    $body = file_get_contents(__DIR__ . '/mocks/insightly_api/getEmails-response.json');
+
+    $api = $this->getAPI(200, $body);
+    $result = $api->getEmails();
+
+    $this->assertEquals(json_decode($body), $result);
+    $this->assertCount(100, $result);
+  }
+  public function test_getEmails_response_can_be_limited_to_x_results() {
+    $body = file_get_contents(__DIR__ . '/mocks/insightly_api/getEmails-limited-response.json');
+
+    $api = $this->getAPI(200, $body);
+    $result = $api->getEmails('?top=5');
+
+    $this->assertCount(5, $result);
+  }
+  public function test_getEmails_response_can_be_told_to_skip_x_results() {
+    $correctBody = file_get_contents(__DIR__ . '/mocks/insightly_api/getEmails-skipped-response.json');
+    $incorrectBody = file_get_contents(__DIR__ . '/mocks/insightly_api/getEmails-limited-response.json');
+
+    $api = $this->getAPI(200, $correctBody);
+    $result = $api->getEmails('?skip=5&top=5');
+
+    $this->assertEquals(json_decode($correctBody), $result);
+    $this->assertNotEquals(json_decode($incorrectBody), $result);
+  }
+
+  /**
+   * Mock external API calls
+   *
+   * @param int   $status status code to be mocked
+   * @param mixed $body   response body to be mocked
+   *
+   * @return Acme\Http\InsightlyAPI
+   */
+  private function getAPI(int $status, $body = null) {
+    $mock = new MockHandler([new Response($status, [], $body)]);
+    $handler = HandlerStack::create($mock);
+    $client = new Client(['handler' => $handler]);
+
+    return new InsightlyAPI($client);
+  }
+
+}
